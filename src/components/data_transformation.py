e1dbe2c45f1feec4566be92f105b226accadc309
sleepy-kittens-404 <muhamadsami501@gmail.com>
2026-06-18 23:59:15 +0500

    Starting with data transformation

---
+++ src/components/data_transformation.py
@@ -0,0 +1,23 @@
+import sys
+import os
+from dataclasses import dataclass
+import numpy as np 
+import pandas as pd 
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler 
+from src.exception import CustomException
+from src.logger import logging
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config =DataTransformationConfig()
+    def get_data_transformer_object(self):
+        try:
+            pass
+        except
